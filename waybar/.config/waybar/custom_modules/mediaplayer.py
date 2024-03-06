072b8441385a4edd035bf33d4c6eb732d7f49c1b
martin <yang.ruoqi@outlook.com>
2024-03-06 21:49:04 +0800

    waybar: adjust clock format and bandwidth module width

--- waybar/.config/waybar/custom_modules/mediaplayer.py
+++ waybar/.config/waybar/custom_modules/mediaplayer.py
@@ -108,7 +108,7 @@ class PlayerManager:
         current_player = self.get_first_playing_player()
         if current_player is not None:
             self.on_metadata_changed(current_player, current_player.props.metadata)
-        else:    
+        else:
             self.clear_output()
 
     def on_metadata_changed(self, player, metadata, _=None):
@@ -120,7 +120,7 @@ class PlayerManager:
         track_info = ""
         if player_name == "spotify" and "mpris:trackid" in metadata.keys() and ":ad:" in player.props.metadata["mpris:trackid"]:
             track_info = "Advertisement"
-        elif artist is not None and title is not None:
+        elif artist is not None and artist != "":
             track_info = f"{title} ({artist})"
         else:
             track_info = title
