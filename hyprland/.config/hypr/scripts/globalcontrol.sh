473eddd7b8c269214e644a9f73da19ef1b006ca9
martin <yang.ruoqi@outlook.com>
2024-03-20 16:16:42 +0800

    remove unused bash functions; update hyprland config

--- hyprland/.config/hypr/scripts/globalcontrol.sh
+++ hyprland/.config/hypr/scripts/globalcontrol.sh
@@ -1,51 +1,12 @@
 #!/usr/bin/env sh
 
-# wallpaper var
-# EnableWallDcol=0
 ConfDir="${XDG_CONFIG_HOME:-$HOME/.config}"
-# ThemeCtl="$ConfDir/hypr/theme.ctl"
 
 # theme var
-gtkTheme=`gsettings get org.gnome.desktop.interface gtk-theme | sed "s/'//g"`
-gtkMode=`gsettings get org.gnome.desktop.interface color-scheme | sed "s/'//g" | awk -F '-' '{print $2}'`
+# gtkTheme=`gsettings get org.gnome.desktop.interface gtk-theme | sed "s/'//g"`
+# gtkMode=`gsettings get org.gnome.desktop.interface color-scheme | sed "s/'//g" | awk -F '-' '{print $2}'`
 
 # hypr var
 hypr_border=`hyprctl -j getoption decoration:rounding | jq '.int'`
 hypr_width=`hyprctl -j getoption general:border_size | jq '.int'`
 
-# pacman fns
-pkg_installed()
-{
-    local PkgIn=$1
-
-    if pacman -Qi $PkgIn &> /dev/null
-    then
-        #echo "${PkgIn} is already installed..."
-        return 0
-    else
-        #echo "${PkgIn} is not installed..."
-        return 1
-    fi
-}
-
-# get_aurhlpr()
-# {
-#     if pkg_installed yay
-#     then
-#         aurhlpr="yay"
-#     elif pkg_installed paru
-#     then
-#         aurhlpr="paru"
-#     fi
-# }
-
-# check(){
-#     local Pkg_Dep=$(for PkgIn in "$@"; do ! pkg_installed $PkgIn && echo "$PkgIn"; done)
-
-# if [ -n "$Pkg_Dep" ]; then echo -e "$0 Dependencies:\n$Pkg_Dep"
-#     read -p "ENTER to install  (Other key: Cancel): " ans
-#     if [ -z "$ans" ]; then get_aurhlpr ; $aurhlpr -S $Pkg_Dep
-#     else echo "Skipping installation of packages" ;exit 1
-#     fi
-# fi
-# }
