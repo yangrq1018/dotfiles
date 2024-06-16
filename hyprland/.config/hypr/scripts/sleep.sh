0d876b5abfd03e317127fbc6e87da314bf664cad
martin <yang.ruoqi@outlook.com>
2024-06-16 16:24:43 +0800

    update hyprland configuration 1. add idle daemon swayidle script 2. make the active window border more obvious 3. add windowrule for RDP client 4. move keybindings to a single file and source it

---
+++ hyprland/.config/hypr/scripts/sleep.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+# This script starts a daemon for locking, screensaving and
+# suspending, after a period of inactivity.
+
+# Note: The timeout is specified in seconds.
+
+swayidle -w \
+  timeout 300 'pgrep -x swaylock || swaylock -f' \
+  timeout 600 'hyprctl dispatch dpms off' \
+  resume 'hyprctl dispatch dpms on'
