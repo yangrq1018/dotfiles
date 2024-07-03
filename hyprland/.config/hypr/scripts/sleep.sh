1893a5a65869f5cf95f785baf05d35436cb3e347
martin <yang.ruoqi@outlook.com>
2024-07-03 09:48:25 +0800

    fix hyprland.conf for new hyprland version

--- hyprland/.config/hypr/scripts/sleep.sh
+++ hyprland/.config/hypr/scripts/sleep.sh
@@ -6,6 +6,6 @@
 # Note: The timeout is specified in seconds.
 
 swayidle -w \
-  timeout 300 'pgrep -x swaylock || swaylock -f' \
-  timeout 600 'hyprctl dispatch dpms off eDP-1' \
+  timeout 600 'pgrep -x swaylock || swaylock -f' \
+  timeout 1200 'hyprctl dispatch dpms off eDP-1' \
   resume 'hyprctl dispatch dpms on eDP-1'
